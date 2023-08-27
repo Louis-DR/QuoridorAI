fca4d115d30e94a0b9c73f3171e5068e6fced005
Evan DUFRAISSE <evan.dufraisse@cea.fr>
2023-08-27 22:59:38 +0200

    fix: syntax changes

--- src/CorridorAgent.cpp
+++ src/CorridorAgent.cpp
@@ -13,21 +13,21 @@ void CorridorAgent::nextMove(QuoridorBoard game)
 {
 }
 
-int CorridorAgent::minimax(QuoridorBoard game, int depth, bool is_first_player)
+uint8_t CorridorAgent::minimax(QuoridorBoard game, int depth, bool is_first_player)
 {
     return 0;
 }
 
 uint8_t CorridorAgent::evaluationHeuristic(QuoridorBoard game)
 {
-    graph = Graph(81);
+    Graph graph = Graph(81);
     Player player1 = game.players[0];
     Player player2 = game.players[1];
 
     uint8_t position_node_player1 = graph.coords2NodeId(player1.position_x, player1.position_y);
-    uint8_t position_node_player1 = graph.coords2NodeId(player2.position_x, player2.position_y);
+    uint8_t position_node_player2 = graph.coords2NodeId(player2.position_x, player2.position_y);
     uint8_t min_distance_player1 = graph.getMinDistance(position_node_player1, true);
-    uint8_t min_distance_player2 = graph.getMinDistance(position_node_player1, false);
+    uint8_t min_distance_player2 = graph.getMinDistance(position_node_player2, false);
     uint8_t num_barriers_player1 = player1.barriers_left;
     uint8_t num_barriers_player2 = player2.barriers_left;
 
