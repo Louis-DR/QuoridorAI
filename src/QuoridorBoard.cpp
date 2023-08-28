14d6a09d260fe7fd21402b6f2a8e7c8018fbce72
Louis Duret-Robert <louisduret@gmail.com>
2023-08-28 09:09:53 +0200

    Fix ambiguiy for std::cout

--- src/QuoridorBoard.cpp
+++ src/QuoridorBoard.cpp
@@ -20,45 +20,45 @@ QuoridorBoard::~QuoridorBoard() {}
 
 void QuoridorBoard::print() {
   // Top row
-  cout << "  ┏━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┓\n";
+  std::cout << "  ┏━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┯━━━┓\n";
   // Iterate over rows from top to bottom
   for (int y = 8; y >= 0; --y) {
     // Left edge
-    cout << y <<" ┃";
+    std::cout << y <<" ┃";
     // Iterate over columns from left to right
     for (int x = 0; x < 9; ++x) {
       // Draw player piece
-      if (x == players[0].position_x && y == players[0].position_y) cout << " ○ ";
-      else if (x == players[1].position_x && y == players[1].position_y) cout << " ● ";
-      else cout << "   ";
+      if (x == players[0].position_x && y == players[0].position_y) std::cout << " ○ ";
+      else if (x == players[1].position_x && y == players[1].position_y) std::cout << " ● ";
+      else std::cout << "   ";
       // If not at the last column, draw the veritcal barrier
       if (x != 8) {
-        if ((y != 0 && barriers.vertical[x][y-1]) || (y != 8 && barriers.vertical[x][y])) cout << "┃";
-        else cout << " ";
+        if ((y != 0 && barriers.vertical[x][y-1]) || (y != 8 && barriers.vertical[x][y])) std::cout << "┃";
+        else std::cout << " ";
       }
     }
     // Right edge
-    cout << "┃\n";
+    std::cout << "┃\n";
 
     // Draw the horizontal edge
     if (y != 0) {
       // Left edge
-      cout << "  ┠";
+      std::cout << "  ┠";
       // Iterate over columns from left to right
       for (int x = 0; x < 9; ++x) {
         // Draw the veritcal barrier
-        if ((x != 0 && barriers.horizontal[x-1][y-1]) || (x != 8 && barriers.horizontal[x][y-1])) cout << "╼━╾";
-        else cout << "╴ ╶";
+        if ((x != 0 && barriers.horizontal[x-1][y-1]) || (x != 8 && barriers.horizontal[x][y-1])) std::cout << "╼━╾";
+        else std::cout << "╴ ╶";
         // Draw the cross point
-        if (x != 8) cout << "┼";
+        if (x != 8) std::cout << "┼";
       }
       // Right edge
-      cout << "┨\n";
+      std::cout << "┨\n";
     }
   }
   // Bottom row
-  cout << "  ┗━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┛\n";
-  cout << "    0   1   2   3   4   5   6   7   8  " << endl;
+  std::cout << "  ┗━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┷━━━┛\n";
+  std::cout << "    0   1   2   3   4   5   6   7   8  " << endl;
 }
 
 Array2D<Array2D<bool,9>,9> QuoridorBoard::get_adjacencyTables() {
@@ -100,7 +100,7 @@ Array2D<Array2D<bool,9>,9> QuoridorBoard::get_adjacencyTables() {
 }
 
 void QuoridorBoard::startInteractiveMode() {
-  cout << "Starting interactive mode." << endl;
+  std::cout << "Starting interactive mode." << endl;
   string cmd_str;
   stringstream cmd_stream;
   vector<string> cmd_split;
@@ -120,43 +120,43 @@ void QuoridorBoard::startInteractiveMode() {
     string cmd_op = cmd_split[0];
     size_t cmd_len = cmd_split.size();
     if (cmd_op == "quit") {
-      cout << "Quitting interactive mode." << endl;
+      std::cout << "Quitting interactive mode." << endl;
       break;
     // } else if (cmd_op == "") {
     // } else if (cmd_op == "") {
     // } else if (cmd_op == "") {
     // } else if (cmd_op == "") {
     } else {
-      cout << "ERROR: Unknown command " << cmd_op << "." << endl;
+      std::cout << "ERROR: Unknown command " << cmd_op << "." << endl;
     }
   }
-  cout << "Interactive mode finished." << endl;
+  std::cout << "Interactive mode finished." << endl;
 }
 
 void QuoridorBoard::debug_checkInvalidStates() {
-  cout << "DEBUG: Checking for invalid board states." << endl;
+  std::cout << "DEBUG: Checking for invalid board states." << endl;
 
   if ((players[0].position_x == 0 && players[1].position_x) && (players[0].position_y == players[1].position_y))
-    cout << "ERROR: Invalid board state, players have the same position " << players[0].position_x << "-" << players[0].position_y << "." << endl;
+    std::cout << "ERROR: Invalid board state, players have the same position " << players[0].position_x << "-" << players[0].position_y << "." << endl;
 
   for (int x = 0; x < 8; x++) {
     for (int y = 0; y < 8; y++) {
       if (barriers.horizontal[x][y]) {
         if (barriers.vertical[x][y])
-          cout << "ERROR: Invalid board state, both vertical and horizontal barriers at position " << x << "-" << y << endl;
+          std::cout << "ERROR: Invalid board state, both vertical and horizontal barriers at position " << x << "-" << y << endl;
         if (x != 7 && barriers.horizontal[x+1][y])
-          cout << "ERROR: Invalid board state, overlapping horizontal barriers at positions " << x << "-" << y << " and " << x+1 << "-" << y << endl;
+          std::cout << "ERROR: Invalid board state, overlapping horizontal barriers at positions " << x << "-" << y << " and " << x+1 << "-" << y << endl;
       }
       else if (barriers.vertical[x][y]) {
         if (y != 7 && barriers.vertical[x][y+1])
-          cout << "ERROR: Invalid board state, overlapping vertical barriers at positions " << x << "-" << y << " and " << x << "-" << y+1 << endl;
+          std::cout << "ERROR: Invalid board state, overlapping vertical barriers at positions " << x << "-" << y << " and " << x << "-" << y+1 << endl;
       }
     }
   }
 }
 
 void QuoridorBoard::debug_clearBarriers() {
-  cout << "DEBUG: Clearing the barriers." << endl;
+  std::cout << "DEBUG: Clearing the barriers." << endl;
   barriers.vertical.fill({});
   barriers.horizontal.fill({});
 }
@@ -164,7 +164,7 @@ void QuoridorBoard::debug_clearBarriers() {
 void QuoridorBoard::debug_setCornerHorizontalBarriers() {
   debug_clearBarriers();
 
-  cout << "DEBUG: Placing only horizontal barriers at the corners." << endl;
+  std::cout << "DEBUG: Placing only horizontal barriers at the corners." << endl;
 
   barriers.horizontal[0][0] = true;
   barriers.horizontal[0][7] = true;
@@ -175,7 +175,7 @@ void QuoridorBoard::debug_setCornerHorizontalBarriers() {
 void QuoridorBoard::debug_setCornerVerticalBarriers() {
   debug_clearBarriers();
 
-  cout << "DEBUG: Placing only vertical barriers at the corners." << endl;
+  std::cout << "DEBUG: Placing only vertical barriers at the corners." << endl;
 
   barriers.vertical[0][0] = true;
   barriers.vertical[0][7] = true;
@@ -186,7 +186,7 @@ void QuoridorBoard::debug_setCornerVerticalBarriers() {
 void QuoridorBoard::debug_setRandomBarriers() {
   debug_clearBarriers();
 
-  cout << "DEBUG: Placing barriers at random locations." << endl;
+  std::cout << "DEBUG: Placing barriers at random locations." << endl;
 
   random_device randseed;
   mt19937 randgen{randseed()};
@@ -206,7 +206,7 @@ void QuoridorBoard::debug_setRandomBarriers() {
 }
 
 void QuoridorBoard::debug_setRandomPlayerPositions() {
-  cout << "DEBUG: Placing the players at random positions." << endl;
+  std::cout << "DEBUG: Placing the players at random positions." << endl;
 
   random_device randseed;
   mt19937 randgen{randseed()};
